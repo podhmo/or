68a1170cb3c1328438e65cc3aa42a2f896739969
podhmo <ababjam61+github@gmail.com>
2022-07-01 22:43:48 +0900

    test: add ng test

--- or_test.go
+++ or_test.go
@@ -11,19 +11,52 @@ type DB struct {
 	Name string
 }
 
+var errEmptyString = fmt.Errorf("must not be empty string")
+var emptyString = ""
+
 func GetDB(name string) (*DB, error) {
-	if name == "" {
-		return nil, fmt.Errorf("must not be empty string")
+	if name == emptyString {
+		return nil, errEmptyString
 	}
 	return &DB{Name: name}, nil
 }
 
+type FakeTB struct {
+	*testing.T
+
+	fmt    string
+	args   []interface{}
+	called bool
+}
+
+func (tb *FakeTB) Fatalf(fmt string, args ...interface{}) {
+	tb.fmt = fmt
+	tb.args = args
+	tb.called = true
+}
+
 func TestFatal(t *testing.T) {
 	t.Run("ok", func(t *testing.T) {
-		got := or.Fatal(GetDB("foo"))(t)
+		got := or.Fatal(GetDB("foo"))(t) // ok
 		want := &DB{Name: "foo"}
+
 		if want, got := want.Name, got.Name; want != got {
-			t.Errorf("Fatal() DB.Name want=%q but got=%q", want, got)
+			t.Errorf("Fatal(), DB.Name want=%q but got=%q", want, got)
+		}
+	})
+
+	t.Run("ng", func(t *testing.T) {
+		fake := &FakeTB{T: t}
+		_ = or.Fatal(GetDB(emptyString))(fake) // ng
+
+		if !fake.called {
+			t.Fatalf("Fatal(), must be failed, but successed")
+		}
+
+		want := fmt.Sprintf(or.FormatText, &DB{}, errEmptyString) // %T %+v
+		got := fmt.Sprintf(fake.fmt, fake.args...)
+		if want != got {
+			t.Errorf("Fatal(), error message is want=%q but got=%q", want, got)
 		}
 	})
 }

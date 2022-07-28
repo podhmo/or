191a246a1f1300c24048d354d3b0cf028c9ba0f5
podhmo <ababjam61+github@gmail.com>
2022-07-28 09:21:27 +0900

    feat: using testing.T.Cleanup()

--- or.go
+++ or.go
@@ -3,6 +3,7 @@ package or
 import "testing"
 
 var FormatText string = "accessing fail (%T): %+v"
+var FormatTextCleanupE string = "accessing fail (%T), cleanup: %+v"
 
 func Fatal[T any](ob T, err error) func(t testing.TB) T {
 	return func(t testing.TB) T {
@@ -14,15 +15,57 @@ func Fatal[T any](ob T, err error) func(t testing.TB) T {
 	}
 }
 
-func FatalWithCleanup[T any](ob T, cleanup func(), err error) func(t testing.TB) (T, func()) {
-	return func(t testing.TB) (T, func()) {
+func FatalWithCleanup[T any](ob T, cleanup func(), err error) func(t testing.TB) T {
+	return func(t testing.TB) T {
 		t.Helper()
+		if cleanup != nil {
+			t.Cleanup(cleanup)
+		}
 		if err != nil {
 			t.Fatalf(FormatText, ob, err)
 		}
-		if cleanup == nil {
-			cleanup = func() {}
+		return ob
+	}
+}
+
+func WithCleanup[T any](ob T, cleanup func()) func(t testing.TB) T {
+	return func(t testing.TB) T {
+		t.Helper()
+		if cleanup != nil {
+			t.Cleanup(cleanup)
 		}
-		return ob, cleanup
+		return ob
+	}
+}
+func FatalWithCleanupE[T any](ob T, cleanup func() error, err error) func(t testing.TB) T {
+	return func(t testing.TB) T {
+		t.Helper()
+		if cleanup != nil {
+			t.Cleanup(func() {
+				t.Helper()
+				if err := cleanup(); err != nil {
+					t.Fatalf(FormatTextCleanupE, ob, err)
+				}
+			})
+		}
+		if err != nil {
+			t.Fatalf(FormatText, ob, err)
+		}
+		return ob
+	}
+}
+
+func WithCleanupE[T any](ob T, cleanup func() error) func(t testing.TB) T {
+	return func(t testing.TB) T {
+		t.Helper()
+		if cleanup != nil {
+			t.Cleanup(func() {
+				t.Helper()
+				if err := cleanup(); err != nil {
+					t.Fatalf(FormatTextCleanupE, ob, err)
+				}
+			})
+		}
+		return ob
 	}
 }
